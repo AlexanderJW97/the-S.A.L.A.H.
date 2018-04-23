29a05de82113e375950e4c2ecc405c32c653f23c
Alexander Woolston <alexwoolston97@gmail.com>
2018-04-23 16:13:52 +0100

    small changes to attendance screen

--- FinalYearProject/attendanceScreen.Designer.cs
+++ FinalYearProject/attendanceScreen.Designer.cs
@@ -29,15 +29,12 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
-            this.displayMeetingsDGV = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.meetingsChooseGroupCmbBx = new System.Windows.Forms.ComboBox();
-            this.registerBtn = new System.Windows.Forms.Button();
             this.attendanceBtn = new System.Windows.Forms.Button();
             this.statsBtn = new System.Windows.Forms.Button();
             this.cancelNewGroupBtn = new System.Windows.Forms.Button();
             this.editGroupBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.displayMeetingsDGV)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
@@ -49,17 +46,6 @@
             this.button2.TabIndex = 15;
             this.button2.Text = "Update Group List";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // displayMeetingsDGV
-            // 
-            this.displayMeetingsDGV.AllowUserToAddRows = false;
-            this.displayMeetingsDGV.AllowUserToDeleteRows = false;
-            this.displayMeetingsDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.displayMeetingsDGV.Location = new System.Drawing.Point(12, 75);
-            this.displayMeetingsDGV.Name = "displayMeetingsDGV";
-            this.displayMeetingsDGV.RowTemplate.Height = 28;
-            this.displayMeetingsDGV.Size = new System.Drawing.Size(650, 580);
-            this.displayMeetingsDGV.TabIndex = 14;
             // 
             // label1
             // 
@@ -79,27 +65,18 @@
             this.meetingsChooseGroupCmbBx.Size = new System.Drawing.Size(227, 28);
             this.meetingsChooseGroupCmbBx.TabIndex = 9;
             // 
-            // registerBtn
-            // 
-            this.registerBtn.Location = new System.Drawing.Point(669, 75);
-            this.registerBtn.Name = "registerBtn";
-            this.registerBtn.Size = new System.Drawing.Size(305, 50);
-            this.registerBtn.TabIndex = 16;
-            this.registerBtn.Text = "Generate Register";
-            this.registerBtn.UseVisualStyleBackColor = true;
-            // 
             // attendanceBtn
             // 
-            this.attendanceBtn.Location = new System.Drawing.Point(668, 131);
+            this.attendanceBtn.Location = new System.Drawing.Point(669, 75);
             this.attendanceBtn.Name = "attendanceBtn";
             this.attendanceBtn.Size = new System.Drawing.Size(305, 49);
             this.attendanceBtn.TabIndex = 17;
-            this.attendanceBtn.Text = "Input Attendance";
+            this.attendanceBtn.Text = "Complete Register For Meeting";
             this.attendanceBtn.UseVisualStyleBackColor = true;
             // 
             // statsBtn
             // 
-            this.statsBtn.Location = new System.Drawing.Point(669, 186);
+            this.statsBtn.Location = new System.Drawing.Point(669, 130);
             this.statsBtn.Name = "statsBtn";
             this.statsBtn.Size = new System.Drawing.Size(305, 49);
             this.statsBtn.TabIndex = 18;
@@ -135,14 +112,11 @@
             this.Controls.Add(this.editGroupBtn);
             this.Controls.Add(this.statsBtn);
             this.Controls.Add(this.attendanceBtn);
-            this.Controls.Add(this.registerBtn);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.displayMeetingsDGV);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.meetingsChooseGroupCmbBx);
             this.Name = "attendanceScreen";
             this.Text = "Attendance Screen";
-            ((System.ComponentModel.ISupportInitialize)(this.displayMeetingsDGV)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,10 +125,8 @@
         #endregion
 
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView displayMeetingsDGV;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox meetingsChooseGroupCmbBx;
-        private System.Windows.Forms.Button registerBtn;
         private System.Windows.Forms.Button attendanceBtn;
         private System.Windows.Forms.Button statsBtn;
         private System.Windows.Forms.Button cancelNewGroupBtn;
